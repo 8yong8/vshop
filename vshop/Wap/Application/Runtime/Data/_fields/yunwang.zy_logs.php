1c4ea079b4eca7f0a27320e8f6df91fd8ed3a375
8yong8 <8yong8@163.com>
2016-02-12 01:44:22 +0800

    系统

--- vshop/Wap/Application/Runtime/Data/_fields/yunwang.zy_logs.php
+++
@@ -1,1 +0,0 @@
-a:5:{i:0;s:2:"id";i:1;s:3:"msg";i:2;s:11:"create_time";s:3:"_pk";s:2:"id";s:5:"_type";a:3:{s:2:"id";s:16:"int(10) unsigned";s:3:"msg";s:11:"varchar(30)";s:11:"create_time";s:16:"int(10) unsigned";}}
--- vshop/Wap/Application/Runtime/Data/_fields/yunwang.zy_pmlist.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:7:"pm_type";i:2;s:5:"pm_id";i:3;s:8:"order_id";i:4;s:11:"create_time";i:5;s:6:"status";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:16:"int(10) unsigned";s:7:"pm_type";s:8:"char(30)";s:5:"pm_id";s:7:"int(10)";s:8:"order_id";s:11:"varchar(64)";s:11:"create_time";s:16:"int(10) unsigned";s:6:"status";s:10:"tinyint(1)";}}
